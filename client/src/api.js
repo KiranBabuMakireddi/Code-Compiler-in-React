cd531107327a27075cc3bc7c06186cb64bcb80f6
KiranBbauMakireddi <kiranbabumakireddi@gmail.com>
2025-04-10 09:06:56 +0530

    implemented super in frontend

--- client/src/api.js
+++ client/src/api.js
@@ -1,15 +1,15 @@
 import axios from 'axios';
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const compileCode = async ({ source_code, language_id }) => {
   try {
-    const res = await axios.post('http://localhost:5000/api/compile', {
+    const res = await axios.post(`${BASE_URL}/api/compile`, {
       source_code,
       language_id,
     });
-
     return res.data;
   } catch (error) {
-    console.error('Error compiling code:', error);
+    console.error("Error compiling code:", error);
     throw error;
   }
 };
